66ae28ada0cc357a76d9a037eb3ae4a408ba3229
mauryanitish <m.nitishkumar786@gmail.com>
2023-07-10 14:58:07 +0530

    Adds functionality
    Add validations on name ,rollno ,height

--- app/src/main/java/com/nitish/linearlayout/MainActivity.kt
+++ app/src/main/java/com/nitish/linearlayout/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.nitish.linearlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var etName:EditText?=null
+    var etRollNo:EditText?=null
+    var etHeight:EditText?=null
+    var btnValidate:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etName = findViewById(R.id.etName)
+        etRollNo = findViewById(R.id.etRollNo)
+        etHeight = findViewById(R.id.etHeight)
+        btnValidate = findViewById(R.id.btnValidate)
+
+        btnValidate?.setOnClickListener {
+            if (etName?.text.toString().isNullOrEmpty()){
+                etName?.error = "Enter your name"
+            }
+            else if (etRollNo?.text.toString().isNullOrEmpty()){
+                etRollNo?.error = "Enter your roll no"
+            }
+            else if(etHeight?.text.toString().isNullOrEmpty()){
+                etHeight?.error = "Enter your height"
+            }
+            else{
+                Toast.makeText(this, "Validate values", Toast.LENGTH_SHORT).show()
+
+                var intent = Intent(this,SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
+
 }
